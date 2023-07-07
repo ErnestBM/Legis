030350a9abe9e3d44ef66b0356b0b1d1fdd1a157
Ernest Benedictus Manurung <ernestbenedictusmanurung@beem.local>
2023-07-07 17:55:40 +0700

    scrape #3

--- src/scraping.py
+++ src/scraping.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def simple_scrape(url):
     result = requests.get(url)
-    article = BeautifulSoup(result.text, "html.parser")
-    tag_a = article.find_all("a")
-    hyp = [link.get("href") for link in tag_a]
+    article = BeautifulSoup(result.content, "html.parser")
+    # tag_a = article.find_all("a")
+    # hyp = [link.get("href") for link in tag_a]
 
-    for link in hyp:
-        print("https://id.wikipedia.org" + str(link))
+    # for link in hyp:
+    #     # print("https://en.wikipedia.org" + str(link))
+    #     print(link)
+
+    tag_a = article.find_all("a", title=re.compile(r'.'), href=re.compile(r'/wiki+'))
+    for i in tag_a:
+        print (i.text)
 
 q = input("Your Wikipedia page: ")
 simple_scrape(q)
