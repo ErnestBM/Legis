f45221819a5a6ed4277b7d832451374828f412d9
Ernest Benedictus Manurung <ernestbenedictusmanurung@beem.local>
2023-07-10 11:53:53 +0700

    surfer #1

--- src/scraping.py
+++ src/scraping.py
@@ -5,16 +5,14 @@ import re
 def simple_scrape(url):
     result = requests.get(url)
     article = BeautifulSoup(result.content, "html.parser")
-    # tag_a = article.find_all("a")
-    # hyp = [link.get("href") for link in tag_a]
-
-    # for link in hyp:
-    #     # print("https://en.wikipedia.org" + str(link))
-    #     print(link)
-
-    tag_a = article.find_all("a", title=re.compile(r'.'), href=re.compile(r'/wiki+'))
-    for i in tag_a:
-        print (i.text)
+    related_with = []
+    for hyp in article.find_all('a'):
+        href_links = hyp.get('href')
+        if href_links and href_links.startswith('/wiki/'):
+            related_with.append('https://en.wikipedia.org' + str(href_links))
+    
+    for line in related_with:
+        print(line)
 
 q = input("Your Wikipedia page: ")
 simple_scrape(q)
--- src/surfer.py
+++ src/surfer.py
@@ -1,17 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
-
-def relationship(hyperlink):
-    related_with = []
-    return related_with
+import re
     
 def wiki_surf(source: str, destination: str):
-    art_log = []
-    while (source != destination):
-        wiki_surf(source, destination)
-        art_log.append(destination)
-    return art_log
+    source_relationship = simple_scrape(source)
+    for hyp in source_relationship:
+        if str(destination) in source_relationship:
+            print('Destination URL ' + str(destination) + ' is reached.') #base case
+            break
+        else: 
+            wiki_surf(hyp, destination)
+            break
 
-p = input('Source URL: ')
-q = input('Destination URL: ')
-wiki_surf(p, q)
+def simple_scrape(url):
+    result = requests.get(url)
+    article = BeautifulSoup(result.content, "html.parser")
+    related_with = []
+    for hyp in article.find_all('a'):
+        href_links = hyp.get('href')
+        if href_links and href_links.startswith('/wiki/'):
+            related_with.append('https://en.wikipedia.org' + str(href_links))
+    return related_with
+
+def main():
+    p = input('Source URL: ')
+    q = input('Destination URL: ')
+    wiki_surf(p, q)
+
+main()
