826964c73137c56e67d417107ddb0b25202d87f9
Ernest Benedictus Manurung <ernestbenedictusmanurung@beem.local>
2023-07-19 13:30:01 +0700

    neo4j #2

--- graph.py
+++ graph.py
@@ -1,21 +1,21 @@
 from neo4j import Auth, GraphDatabase
+
 #establishing connection to local dbms (to default dbms)
 graph_db=GraphDatabase.driver(uri='bolt://localhost:7687',auth=('neo4j','Legis123'))
-
 #establishing a session
 session=graph_db.session()
 
 #execute queries
-inp = input('Insert the name of the student: ')
-q1 = 'MATCH (s1:Student {name: "' + str(inp) + '"})-->(s2:Student) RETURN s2.name' #concatenate strings because f'{}' does not work
-q2 = 'MATCH (s1:Student {name: "' + str(inp) + '"})-[r]->(s2:Student) RETURN type(r)'
-name = session.run(q1)
-rel = session.run(q2)
-for vertex in rel:
-    print(vertex)
+def main():
+    inp = input('Insert the name of the student: ')
+    q1 = '''MATCH (s1:Student {name: $inp})-[r]->(s:Student) 
+            RETURN s.name AS name, type(r) AS relationship'''
+    name = session.run(q1, inp = inp)
+
     for edge in name:
-        print(edge)
-    print('')
+        n = edge.get('name')
+        r = edge.get('relationship')
+        print(n + ', ' + r)
+        print('')
 
-
-
+main()
